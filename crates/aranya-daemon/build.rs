708c2494cfbf248237a7feeb3b2f4a8ff8acb49b
Jonathan Dygert <jdygert@spideroak.com>
2024-10-16 17:02:06 -0400

    Skip policy-ifgen-build when building for docs.rs (#14)

--- crates/aranya-daemon/build.rs
+++ crates/aranya-daemon/build.rs
@@ -1,5 +1,10 @@
+use std::env;
+
 fn main() {
     println!("cargo:rerun-if-changed=src/policy.md");
-    aranya_policy_ifgen_build::generate("src/policy.md", "src/policy.rs")
-        .expect("expected policy-ifgen to generate policy.rs");
+    // docs.rs build sets the source as readonly.
+    if env::var("DOCS_RS").as_deref() != Ok("1") {
+        aranya_policy_ifgen_build::generate("src/policy.md", "src/policy.rs")
+            .expect("expected policy-ifgen to generate policy.rs");
+    }
 }
