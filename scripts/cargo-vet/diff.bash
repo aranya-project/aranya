7b9d00d3331379bf12598726e7855d0993d9d983
gknopf <gknopf@spideroak.com>
2025-03-04 15:59:42 -0700

    diff script

--- scripts/cargo-vet/diff.bash
+++ scripts/cargo-vet/diff.bash
@@ -8,5 +8,5 @@ fi
 
 pushd ../../
 cargo vet check | grep "cargo vet diff" | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}' | while read -r line ; do eval "$line" ; done
-cargo vet check | grep "cargo vet inspect" | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}' | while read -r line ; do eval "$line" ; done
+cargo vet check | grep "cargo vet inspect" | awk '{print $1 " " $2 " " $3 " " $4 " " $5}' | while read -r line ; do eval "$line" ; done
 popd
