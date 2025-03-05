25149ec7d45f54ac48db4499385799946b1739b4
gknopf <gknopf@spideroak.com>
2025-03-04 17:05:24 -0700

    certify

--- scripts/cargo-vet/diff.bash
+++ scripts/cargo-vet/diff.bash
@@ -6,7 +6,23 @@ if command -v shellcheck; then
 	shellcheck "${0}"
 fi
 
+# cargo vet diff
+diff=${DIFF:-1}
+# cargo vet certify
+certify=${CERTIFY:-1}
+
 pushd ../../
-cargo vet check | grep "cargo vet diff" | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}' | while read -r line ; do eval "$line" ; done
-cargo vet check | grep "cargo vet inspect" | awk '{print $1 " " $2 " " $3 " " $4 " " $5}' | while read -r line ; do eval "$line" ; done
+cargo vet check | while read -r line ; do
+    echo "$line"
+    # diff
+    if [ "${diff}" == "1" ]; then
+        eval "$(echo "$line" | grep "cargo vet diff" | awk '{print $1 " " $2 " " $3 " " $4 " " $5 " " $6}')"
+        eval "$(echo "$line" | grep "cargo vet inspect" | awk '{print $1 " " $2 " " $3 " " $4 " " $5}')"
+    fi
+    # certify
+    if [ "${certify}" == "1" ]; then
+        eval "$(echo "$line" | grep "cargo vet diff" | awk '{print "cargo vet certify --accept-all " $4 " " $5 " " $6}')"
+        eval "$(echo "$line" | grep "cargo vet inspect" | awk '{print "cargo vet certify --accept-all " $4 " " $5}')"
+    fi
+done
 popd
