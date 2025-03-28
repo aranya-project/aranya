f8a27ac254a3e761d112071e26aad4919f696d91
Declan Johnson <djohnson@spideroak.com>
2025-03-28 16:12:28 -0500

    update link

--- crates/aranya-daemon-api/src/lib.rs
+++ crates/aranya-daemon-api/src/lib.rs
@@ -7,12 +7,7 @@
 //!
 //! [`tarpc`]: https://crates.io/crates/tarpc
 //!
-//! For more information, refer to:
-//! - The [`aranya-client` README]
-//! - The [`aranya-daemon` README]
-//!
-//! [`aranya-client` README]: https://github.com/aranya-project/aranya/tree/main/crates/aranya-client/README.md
-//! [`aranya-daemon` README]: https://github.com/aranya-project/aranya/tree/main/crates/aranya-daemon/README.md
+//! For more information, refer to: https://docs.rs/aranya-client/latest/aranya_client/
 
 mod service;
 
