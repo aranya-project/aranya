5da5b33479713c13aca7fd9306e41b449bd6fc0f
jdygert-spok <jdygert@spideroak.com>
2025-08-20 19:02:38 +0000

    Deploying to gh-pages from @ aranya-project/aranya@c8a71eefe242e06799af73bb3d9b03808e066928 🚀

---
+++ capi/v2.0.0/search/defines_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['aranya_5fid_5flen_0',['ARANYA_ID_LEN',['../aranya-client_8h.html#a31f98e4be1c272407d37161dbd321920',1,'aranya-client.h']]],
+  ['aranya_5fid_5fstr_5flen_1',['ARANYA_ID_STR_LEN',['../aranya-client_8h.html#a30bc46012189c703b990bc0d6245f5e3',1,'aranya-client.h']]],
+  ['aranya_5fseed_5fikm_5flen_2',['ARANYA_SEED_IKM_LEN',['../aranya-client_8h.html#a00c1859da30ca9d9b96a09aed0998a69',1,'aranya-client.h']]]
+];
---
+++ capi/v2.0.0/search/enums_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['aranyaaqcchanneltype_0',['AranyaAqcChannelType',['../aranya-client_8h.html#a5c7b0d3c242a02e189f01ecc39ae0311',1,'aranya-client.h']]],
+  ['aranyachanop_1',['AranyaChanOp',['../aranya-client_8h.html#ae327ccde00be772aaef1361231d36495',1,'aranya-client.h']]],
+  ['aranyaerror_2',['AranyaError',['../aranya-client_8h.html#ae17c03176fbf2b753be8db7e4007d558',1,'aranya-client.h']]],
+  ['aranyarole_3',['AranyaRole',['../aranya-client_8h.html#af8e2c410a381e31b630764c9341c4727',1,'aranya-client.h']]]
+];
