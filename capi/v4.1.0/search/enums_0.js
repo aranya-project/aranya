b8d0b85c7d69fab975c5ca7e469c5b0a7c1aa7d7
gknopf-aranya <gknopf@spideroak.com>
2026-01-07 17:05:09 +0000

    Deploying to gh-pages from @ aranya-project/aranya@94f025ffa1dc04cad0a6a2842107b15a14e514a2 🚀

---
+++ capi/v4.1.0/search/enums_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['aranyachanop_0',['AranyaChanOp',['../aranya-client_8h.html#ae327ccde00be772aaef1361231d36495',1,'aranya-client.h']]],
+  ['aranyaerror_1',['AranyaError',['../aranya-client_8h.html#ae17c03176fbf2b753be8db7e4007d558',1,'aranya-client.h']]]
+];
