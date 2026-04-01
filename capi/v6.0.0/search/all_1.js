4b087929648ca84c43049549555828676bb0cf85
gknopf-aranya <gknopf@spideroak.com>
2026-04-01 19:41:52 +0000

    Deploying to gh-pages from @ aranya-project/aranya@2478b2eedac393c5eb59313744eb06fee45f1250 🚀

---
+++ capi/v6.0.0/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['c_20api_0',['Aranya C API',['../index.html',1,'']]]
+];
---
+++ capi/v6.0.0/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['aranya_2dclient_2eh_0',['aranya-client.h',['../aranya-client_8h.html',1,'']]]
+];
